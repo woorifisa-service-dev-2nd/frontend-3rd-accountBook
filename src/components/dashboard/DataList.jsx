6a65afb923e0c4862c114e64a851397f1ffd9712
yueunkim <yueunkim0605@gmail.com>
2023-12-15 10:20:14 +0900

    Feat: json-server연결해서 데이터 삭제 기능 구현d

--- src/components/dashboard/DataList.jsx
+++ src/components/dashboard/DataList.jsx
@@ -2,34 +2,50 @@ import { useState, useEffect, useContext } from 'react';
 import { Flex, Button } from '@chakra-ui/react';
 import DataTable from './DataTable';
 import { AccountContext } from '../../App';
+import { URL } from '../../App';
+
 
 const DataList = ({ updatedData }) => {
   const [data, setData] = useContext(AccountContext);
   const [checkedItems, setCheckedItems] = useState([]);
 
   // NOTE : 임시 주석
-  // // 삭제
-  // const handleDelete = (id) => {
-  //   const newData = data.filter((item) => item.id !== id);
-  //   setData(newData);
-  //   setCheckedItems(newData.map(() => false));
-  // };
-  // // 체크박스
-  // const toggleAllCheckboxes = (isChecked) => {
-  //   setCheckedItems(Array(data.length).fill(isChecked));
-  // };
-  // const toggleSingleCheckbox = (index, isChecked) => {
-  //   const newCheckedItems = [...checkedItems];
-  //   newCheckedItems[index] = isChecked;
-  //   setCheckedItems(newCheckedItems);
-  // };
-  // const isAnyItemChecked = () => checkedItems.some(Boolean);
-  // // 모두 삭제
-  // const handleDeleteChecked = () => {
-  //   const newData = data.filter((item, index) => !checkedItems[index]);
-  //   setData(newData);
-  //   setCheckedItems(newData.map(() => false));
-  // };
+  // 삭제
+  const handleDelete = (id) => {
+    const newData = data.filter((item) => item.id !== id);
+    setData(newData);
+    setCheckedItems(newData.map(() => false));
+
+    fetchSave(id);
+  };
+  // 체크박스
+  const toggleAllCheckboxes = (isChecked) => {
+    setCheckedItems(Array(data.length).fill(isChecked));
+  };
+  const toggleSingleCheckbox = (index, isChecked) => {
+    const newCheckedItems = [...checkedItems];
+    newCheckedItems[index] = isChecked;
+    setCheckedItems(newCheckedItems);
+  };
+  const isAnyItemChecked = () => checkedItems.some(Boolean);
+  // 모두 삭제
+  const handleDeleteChecked = () => {
+    const newData = data.filter((item, index) => !checkedItems[index]);
+    setData(newData);
+    setCheckedItems(newData.map(() => false));
+  };
+
+  const fetchSave = (id) => {
+    fetch(`${URL}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => response.json())
+      .catch((e) => console.log(e));
+  };
+  
   // // 수정
   // const handleEdit = () => {
   //   console.log('edit');
@@ -38,21 +54,21 @@ const DataList = ({ updatedData }) => {
   return (
     <Flex direction="column">
       {/* NOTE : 일괄 삭제 임시 주석 */}
-      {/* <Flex justifyContent="end">
+      <Flex justifyContent="end">
         <Button colorScheme={isAnyItemChecked() ? 'red' : 'gray'} m={4} onClick={handleDeleteChecked} isDisabled={!isAnyItemChecked()}>
           모두 삭제
         </Button>
-      </Flex> */}
+      </Flex>
 
       {/* 데이터 테이블 */}
       <DataTable
         data={data}
         // NOTE : 일괄 삭제 임시 주석
-        // checkedItems={checkedItems}
-        // toggleAllCheckboxes={toggleAllCheckboxes}
-        // toggleSingleCheckbox={toggleSingleCheckbox}
+        checkedItems={checkedItems}
+        toggleAllCheckboxes={toggleAllCheckboxes}
+        toggleSingleCheckbox={toggleSingleCheckbox}
         // handleEdit={handleEdit}
-        // handleDelete={handleDelete}
+        handleDelete={handleDelete}
       />
     </Flex>
   );
